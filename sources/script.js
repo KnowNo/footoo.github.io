0d4c88889b38c0bfa41e1867f27e869116a6da60
Soufii <abbshr@outlook.com>
2013-12-08 21:33:25 +0800

    add active to title

---
+++ sources/script.js
@@ -0,0 +1,33 @@
+(function (global) {
+    var logo = document.getElementById('logo');
+    var text = document.createTextNode('');
+    logo.appendChild(text);
+
+    var str = "Welcome to FoOTOo Lab";
+    var timer = setInterval(writer, 500);
+    var count = 0;
+
+    function writer() {
+        if (count == str.length)
+            return flicker();
+        text.textContent += str[count];
+        count++;
+    }
+
+    function flicker() {
+        clearInterval(timer);
+        var exsit = false;
+        var otext = text.textContent;
+        setInterval(callback, 700);
+
+        function callback() {
+            if (exsit) {
+                text.textContent = otext;
+                exsit = false;
+            } else {
+                text.textContent += '_';
+                exsit = true;
+            }
+        }
+    }
+})(this);
